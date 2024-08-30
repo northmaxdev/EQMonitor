171538e0c578457bcbb86b62d8e6459ed96f3010
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-08-31 01:42:14 +0300

    Add Model.Location unit tests

---
+++ EQMonitor.Core.Tests/Model/LocationTests.cs
@@ -0,0 +1,16 @@
+﻿namespace EQMonitor.Core.Tests.Model
+{
+    [TestFixture]
+    public class LocationTests
+    {
+        [Test]
+        public void RejectsBlankDescription()
+        {
+        }
+
+        [Test]
+        public void AcceptsNonBlankDescription()
+        {
+        }
+    }
+}
