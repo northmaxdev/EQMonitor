90e6200f1b9ba8ca67e9e6360182eb6973cac1bf
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-09-01 22:37:05 +0300

    Implement Location unit tests

--- EQMonitor.Core.Tests/Model/LocationTests.cs
+++ EQMonitor.Core.Tests/Model/LocationTests.cs
@@ -1,16 +1,26 @@
-﻿namespace EQMonitor.Core.Tests.Model
+﻿using EQMonitor.Core.Model;
+
+namespace EQMonitor.Core.Tests.Model
 {
     [TestFixture]
     public class LocationTests
     {
-        [Test]
-        public void RejectsBlankDescription()
+        private GeoPoint EiffelTowerCoordinates { get; init; } = new GeoPoint
         {
+            Latitude = 48.8583,
+            Longitude = 2.2945
+        };
+
+        [Test]
+        public void RejectsBlankDescription([Values("", " ", "\t", "\n")] string description) // Can't use string.Empty
+        {
+            Assert.Throws<ArgumentException>(() => new Location(EiffelTowerCoordinates, description));
         }
 
         [Test]
         public void AcceptsNonBlankDescription()
         {
+            Assert.DoesNotThrow(() => new Location(EiffelTowerCoordinates, "Eiffel Tower"));
         }
     }
 }
