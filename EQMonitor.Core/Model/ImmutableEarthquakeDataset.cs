237a810efde12a2d33a9d43f799f5fb7ea8ea130
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-09-28 22:11:03 +0300

    Add ImmutableEarthquakeDataset

---
+++ EQMonitor.Core/Model/ImmutableEarthquakeDataset.cs
@@ -0,0 +1,14 @@
+﻿namespace EQMonitor.Core.Model
+{
+    // TODO: Double-check my understanding of "sealed" (compared to Java)
+    public sealed class ImmutableEarthquakeDataset : IEarthquakeDataset
+    {
+        public Earthquake? Strongest => throw new NotImplementedException();
+
+        public Earthquake? Weakest => throw new NotImplementedException();
+
+        public Earthquake? Latest => throw new NotImplementedException();
+
+        public Earthquake? Earliest => throw new NotImplementedException();
+    }
+}
