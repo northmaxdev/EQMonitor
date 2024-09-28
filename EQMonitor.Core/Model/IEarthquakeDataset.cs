13021087ae22a2b31f4b72ab357987f1c4103a12
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-09-28 21:50:19 +0300

    Add IEarthquakeDataset

---
+++ EQMonitor.Core/Model/IEarthquakeDataset.cs
@@ -0,0 +1,15 @@
+﻿namespace EQMonitor.Core.Model
+{
+    public interface IEarthquakeDataset
+    {
+        public Earthquake? Strongest { get; }
+        public Earthquake? Weakest { get; }
+        public Earthquake? Latest { get; }
+        public Earthquake? Earliest { get; }
+
+        /*
+         * TODO: Average magnitude property
+         * If implemented, should it be a nullable double? I.e., 0.0 or null magnitude for empty datasets?
+         */
+    }
+}
