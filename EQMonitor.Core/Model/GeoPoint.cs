0b57b87cf6a8c1823836779b2820dc8c4449a65f
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-09-28 21:55:13 +0300

    Add TODO's: equals/hashCode unit tests for domain data types

--- EQMonitor.Core/Model/GeoPoint.cs
+++ EQMonitor.Core/Model/GeoPoint.cs
@@ -1,5 +1,6 @@
 ﻿namespace EQMonitor.Core.Model
 {
+    // TODO: Unit tests for equals/hashCode semantics
     // Self-note: longitude is X, latitude is Y
     public readonly record struct GeoPoint(double Longitude, double Latitude);
 }
--- EQMonitor.Core/Model/Location.cs
+++ EQMonitor.Core/Model/Location.cs
@@ -1,4 +1,5 @@
 ﻿namespace EQMonitor.Core.Model
 {
+    // TODO: Unit tests for equals/hashCode semantics
     public record class Location(GeoPoint Coordinates, string? Description);
 }
