0fd6b7e8b2a8e6893f4f52e12b7ef54251cd0708
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-10-01 14:52:39 +0300

    Add IEarthquakeDataset.AverageMagnitude

--- EQMonitor.Core/Model/IEarthquakeDataset.cs
+++ EQMonitor.Core/Model/IEarthquakeDataset.cs
@@ -6,10 +6,12 @@
         public Earthquake? Weakest { get; }
         public Earthquake? Latest { get; }
         public Earthquake? Earliest { get; }
+        public double AverageMagnitude { get; }
 
         /*
-         * TODO: Average magnitude property
-         * If implemented, should it be a nullable double? I.e., 0.0 or null magnitude for empty datasets?
+         * TODO:
+         * Ability to get a subset of the current dataset with some specific criteria, e.g. earthquakes of a single country only
+         * (if this is applicable in the geographical sense)
          */
     }
 }
