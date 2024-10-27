dc5c3ca4b766b7e957fb06c809eb4ca8408192d9
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-10-27 20:56:54 +0200

    Replace IEarthquakeDataset with EarthquakeStats

---
+++ EQMonitor.Core/Model/EarthquakeStats.cs
@@ -0,0 +1,28 @@
+﻿namespace EQMonitor.Core.Model;
+
+public sealed class EarthquakeStats
+{
+    public Earthquake? Strongest { get; }
+    public Earthquake? Weakest { get; }
+    public Earthquake? Latest { get; }
+    public Earthquake? Earliest { get; }
+    public double AverageMagnitude { get; }
+
+    private EarthquakeStats(IEnumerable<Earthquake> dataset)
+    {
+        // https://learn.microsoft.com/en-us/dotnet/fundamentals/code-analysis/quality-rules/ca1851
+        IReadOnlyCollection<Earthquake> datasetAsCollection = dataset.ToList();
+
+        Strongest = datasetAsCollection.MaxBy(earthquake => earthquake.Magnitude);
+        Weakest = datasetAsCollection.MinBy(earthquake => earthquake.Magnitude);
+        Latest = datasetAsCollection.MaxBy(earthquake => earthquake.RegistrationTimestamp);
+        Earliest = datasetAsCollection.MinBy(earthquake => earthquake.RegistrationTimestamp);
+        AverageMagnitude = datasetAsCollection.Average(earthquake => earthquake.Magnitude);
+    }
+
+    // Syntactic sugar for readability
+    public static EarthquakeStats From(IEnumerable<Earthquake> dataset)
+    {
+        return new EarthquakeStats(dataset);
+    }
+}
