66ab41116a5659a3c3fc3576e0c6274d6197323e
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-10-29 01:31:05 +0200

    Add EQMonitor.GUI project

---
+++ EQMonitor.GUI/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using EQMonitor.Core.Earthquake;
+
+namespace EQMonitor.GUI;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        EarthquakeService service = new EarthquakeService(new HttpClient());
+    }
+}
