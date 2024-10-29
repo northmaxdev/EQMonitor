2733fedcf04717fe98dab819b827b1e79b67522e
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-10-29 18:30:30 +0200

    Provide service to MainWindow in WPF entry point

--- EQMonitor.GUI/App.xaml.cs
+++ EQMonitor.GUI/App.xaml.cs
@@ -1,3 +1,19 @@
-﻿namespace EQMonitor.GUI;
+﻿using System.Net.Http;
+using System.Windows;
+using EQMonitor.Core.Earthquake;
 
-public partial class App;
+namespace EQMonitor.GUI;
+
+public partial class App
+{
+    private readonly HttpClient _httpClient = new();
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        EarthquakeService earthquakeService = new(_httpClient);
+        var mainWindow = new MainWindow(earthquakeService);
+        mainWindow.Show();
+    }
+}
