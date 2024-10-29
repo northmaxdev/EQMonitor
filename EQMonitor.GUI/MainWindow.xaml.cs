b593ee53ba36a8535f21dba8220c4b618d6b7a80
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-10-29 18:29:29 +0200

    Remove stuff I'm too lazy to implement in MainWindow

--- EQMonitor.GUI/MainWindow.xaml.cs
+++ EQMonitor.GUI/MainWindow.xaml.cs
@@ -1,21 +1,32 @@
-﻿using EQMonitor.Core.Earthquake;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+using EQMonitor.Core.Earthquake;
 
 namespace EQMonitor.GUI;
 
 public partial class MainWindow
 {
-    public MainWindow()
+    private readonly EarthquakeService _earthquakeService;
+
+    public MainWindow(EarthquakeService earthquakeService)
     {
-        // DatasetView
-        // TimePeriodSelector
-        // FetchDataButton
-        // FetchProgressBar
-        // ViewStatsButton
-        // CsvExportButton
-        // CsvExportProgressBar
+        _earthquakeService = earthquakeService;
 
         InitializeComponent();
-
         TimePeriodSelector.ItemsSource = Enum.GetValues<EarthquakeService.TimePeriod>();
+    }
+
+    // This is safe because EarthquakeStats.From() creates a backing list before iterating
+    [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+    private void FetchDataButton_Click(object sender, RoutedEventArgs e)
+    {
+        var timePeriod = (EarthquakeService.TimePeriod)TimePeriodSelector.SelectedItem;
+
+        IEnumerable<EarthquakeModel> dataset = _earthquakeService.GetData(timePeriod);
+        var stats = EarthquakeStats.From(dataset);
+
+        DatasetView.ItemsSource = dataset;
+        EarthquakeCountComment.Content = $"{stats.Count} earthquakes";
+        AverageMagnitudeComment.Content = $"Avg. magnitude: {stats.AverageMagnitude:F3}";
     }
 }
