ffb1351c5bcc0c3391702abc50425a5ad7afe190
northmaxdev <125212875+northmaxdev@users.noreply.github.com>
2024-10-29 18:36:39 +0200

    EarthquakeModel: custom toString

--- EQMonitor.Core/Earthquake/EarthquakeModel.cs
+++ EQMonitor.Core/Earthquake/EarthquakeModel.cs
@@ -2,4 +2,10 @@
 
 namespace EQMonitor.Core.Earthquake;
 
-public record EarthquakeModel(LocationModel OccurrenceLocation, double Magnitude, DateTimeOffset OccurrenceTimestamp);
+public record EarthquakeModel(LocationModel OccurrenceLocation, double Magnitude, DateTimeOffset OccurrenceTimestamp)
+{
+    public override string ToString()
+    {
+        return $"{Magnitude:F2} {OccurrenceLocation} at {OccurrenceTimestamp}";
+    }
+}
